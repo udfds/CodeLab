ff96479cbc2ace5d03a814652489f0ea8afe2723
Diego <dfds@outlook.com>
2020-03-15 16:06:14 -0200

    Add files via upload

---
+++ experiments/rich-text-editor/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import { TextEditor } from './components'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TextEditor/>
+    </div>
+  );
+}
+
+export default App;
