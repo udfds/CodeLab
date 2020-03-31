770cf4bec0dfcdd9b275a1bdef84e74792047fbd
udfds <dfds@outlook..com>
2020-03-31 18:53:35 -0200

    microblog - part 3

--- experiments/microblog/src/main/java/com/codelab/partymanager/repository/Guests.java
+++
@@ -1,8 +0,0 @@
-package com.codelab.partymanager.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.codelab.partymanager.models.Guest;
-
-public interface Guests extends JpaRepository<Guest, Long> {
-
-}
