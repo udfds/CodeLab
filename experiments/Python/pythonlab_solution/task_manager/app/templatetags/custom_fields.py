9c288ee843818a0434b992e91978deae66a561a6
udfds <dfds@outlook.com>
2020-04-27 14:11:56 -0300

    task-manager - part 4

--- experiments/Python/pythonlab_solution/task_manager/app/templatetags/custom_fields.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.filter(name='addClass')
-def addClass(value, args):
-    return value.as_widget(attrs={'class': args})
--- experiments/Python/pythonlab_solution/task_manager/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('task_list', task_list, name='task_list'),
-    path('task_create', task_create, name='task_create'),
-    path('task_edit/<int:id>', task_edit, name='task_edit'),
-    path('task_delete/<int:id>', task_delete, name='task_delete')
-]
