4a255f169f9aa3cfe772715f5a1793cf5edb5389
udfds <dfds@outlook.com>
2020-07-18 22:55:14 -0300

    NodeJs - Part 1

--- experiments/JavaScript/NodeJs Lab/paradox-api/src/app.js
+++ experiments/JavaScript/NodeJs Lab/paradox-api/src/app.js
@@ -1,17 +1,41 @@
 'use strict'
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
 
-const route = router.get('/', (req, res, next) => {
+// Converter automaticamente em json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const route_get = router.get('/', (_req, res, _next) => {
     res.status(200).send({
         title: 'Paradox API',
-        version: '0.0.1'
+        version: '0.0.2'
     });
 });
 
-app.use('/', route);
+const route_post = router.post('/', (req, res, _next) => {
+    res.status(201).send(req.body);
+});
+
+const route_put = router.put('/:id', (req, res, _next) => {
+    const id = req.params.id;
+    res.status(200).send({
+        id: id,
+        item: req.body
+    });
+});
+
+const roude_delete = router.delete('/:id', (req, res, _next) => {
+    res.status(200).send(req.body);
+});
+
+app.use('/', route_get);
+app.use('/', route_post);
+app.use('/', route_put);
+app.use('/', roude_delete);
 
 module.exports = app;
